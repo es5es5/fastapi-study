2a78e49a2cb6d309a7ff7f89517aa747584db4f2
es5es5 <es5e5s@nate.com>
2022-02-06 17:36:56 +0900

    world timezone

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+db = [{
+    'name': '서울',
+    'timezone': 'Asia/Seoul'
+}]
+
+class City(BaseModel):
+    name: str
+    timezone: str
+
+@app.get("/")
+def root():
+    return { "Hello": "Fast API !" }
+
+@app.get('/cities')
+def getCities():
+    result = []
+
+    print(db)
+    for city in db:
+        print(city)
+        print(city['name'])
+        url = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
+        print(url)
+        r = requests.get(url)
+
+        currentTime = r.json()['datetime']
+        result.append({
+            'name': city['name'],
+            'timezone': city['timezone'],
+            'currentTime': currentTime
+        })
+
+    return result
+
+@app.post('/cities')
+def createCities(city: City):
+    db.append(city.dict())
+
+    print(db)
+
+    return city
